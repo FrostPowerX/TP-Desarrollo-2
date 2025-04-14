ec04c39739bd9babcdeafc65023493eec3c902ba
FrostPowerX <emaguzme@hotmail.com>
2025-04-13 22:53:42 -0300

    Add Boost on Jump

--- Cacho/Assets/Scripts/Player/PlayerMove.cs
+++ Cacho/Assets/Scripts/Player/PlayerMove.cs
@@ -4,13 +4,22 @@ using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
+    [Header("Components")]
     [SerializeField] InputActionAsset inputActions;
     [SerializeField] Rigidbody rb;
     [SerializeField] FloorDetector fDet;
 
+    [Header("Movement Config")]
     [SerializeField] float speed;
     [SerializeField] float sprintSpeed;
+
+    [Header("Jump Config")]
     [SerializeField] float jumpForce;
+    [SerializeField] float divisorJumpForce;
+    [SerializeField] [Range(0f,1f)] float boostJumpDefaultTime;
+
+    [Header("Info")]
+    [SerializeField] float boostJumpTimer;
 
     InputAction move;
     InputAction crouch;
@@ -23,6 +32,7 @@ public class PlayerMove : MonoBehaviour
     float usedSpeed;
 
     bool jumping;
+    bool pressJump;
 
     void Start()
     {
@@ -32,19 +42,61 @@ public class PlayerMove : MonoBehaviour
         sprint = inputActions.FindAction("Sprint");
 
         jump.started += Jump;
+        jump.canceled += CancelBoostJump;
 
         inputActions.Enable();
     }
 
     void Update()
     {
+        boostJumpTimer -= (boostJumpTimer > 0) ? Time.deltaTime : boostJumpTimer;
+
+        pressJump = jump.IsPressed() && boostJumpTimer > 0 && boostJumpTimer <= boostJumpDefaultTime - 0.05;
+
         Move();
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+        Jumping();
+    }
+
+    void Movement()
+    {
+        if (!fDet.OnFloor)
+            return;
+
+        rb.AddForce(movementX, ForceMode.Force);
+        rb.AddForce(movementZ, ForceMode.Force);
+    }
+
+    void Jumping()
+    {
+        if (jumping)
+        {
+            rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+            jumping = false;
+        }
+
+        if (pressJump)
+        {
+            rb.AddForce(new Vector3(0, jumpForce / divisorJumpForce, 0), ForceMode.Impulse);
+        }
+    }
+
+    void CancelBoostJump(InputAction.CallbackContext obj)
+    {
+        boostJumpTimer = 0;
     }
 
     void Jump(InputAction.CallbackContext obj)
     {
         if (fDet.OnFloor)
+        {
             jumping = true;
+            boostJumpTimer = boostJumpDefaultTime;
+        }
     }
 
     void Move()
@@ -53,17 +105,5 @@ public class PlayerMove : MonoBehaviour
 
         movementX = transform.right * usedSpeed * move.ReadValue<Vector2>().x;
         movementZ = transform.forward * usedSpeed * move.ReadValue<Vector2>().y;
-    }
-
-    void FixedUpdate()
-    {
-        rb.AddForce(movementX, ForceMode.Force);
-        rb.AddForce(movementZ, ForceMode.Force);
-
-        if(jumping)
-        {
-            rb.AddForce(new Vector3(0, jumpForce,0), ForceMode.Impulse);
-            jumping = false;
-        }
     }
 }
